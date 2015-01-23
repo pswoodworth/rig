2c0276cb2e248e55da227e015fd018c478661f44
pswoodworth <pswoodworth@gmail.com>
2015-01-23 18:45:40 -0500

    initial

---
+++ rig.js
@@ -0,0 +1,89 @@
+var breakpoint = 600,
+	mobilePositionIndex = 0;
+
+
+function initRig(){
+	$('.rig').wrapInner('<div class="rig-mobile-wrapper"></div>');
+
+	$('.rig-forward').click(function(){
+		if(isMobile()){
+			mobilePositionIndex += 1;
+			updateMobilePosition();
+		};
+	});
+
+	$('.rig-back').click(function(){
+		if(isMobile()){
+			mobilePositionIndex -= 1;
+			updateMobilePosition();
+		};
+	});
+};
+
+function rig(){
+
+	if($(window).width() > breakpoint){
+		// desktop/tablet
+
+		$('.rig, .rig-sidebar, .rig-body, .rig-mobile-wrapper').removeAttr('style');
+
+		var totalSidebarWidth = 0;
+
+		$('.rig-sidebar').each(function(index) {
+			totalSidebarWidth += parseInt($(this).width(), 10);
+		});
+
+		$('.rig-body').width( ($(window).width() - totalSidebarWidth) / $('.rig-body').length );
+
+	}else{
+		// mobile
+
+		$('.rig', '.rig-sidebar, .rig-body, .rig-mobile-wrapper').removeAttr('style');
+
+		$('.rig-mobile-wrapper').width($('.rig-sidebar, .rig-body').length * $(window).width());
+
+		$('.rig, .rig-sidebar, .rig-body').width($(window).width());
+
+		$('.rig-mobile-wrapper').css('left', $(window).width() * -1)
+
+	};
+};
+
+
+function isMobile(){
+	console.log($(window).width() < breakpoint);
+	return $(window).width() < breakpoint;
+};
+
+function updateMobilePosition(){
+	if (isMobile()){
+		$('.rig-mobile-wrapper').css('left',  $(window).width() * -1 * mobilePositionIndex);
+	};
+};
+
+
+
+$(document).ready(function(){
+	initRig();
+	rig();
+	updateMobilePosition();
+});
+
+var resizeTimeout;
+$(window).resize(function(){
+	$('.rig').addClass('resizing');
+	
+	rig();
+	updateMobilePosition();
+
+	clearTimeout(resizeTimeout);
+	resizeTimeout = setTimeout(function(){
+		$('.rig').removeClass('resizing');
+		console.log('resize done');
+	}, 50);
+
+});
+
+
+
+
