c70a643846525508fa08d5d70cd7f1d7f11dcded
pswoodworth <pswoodworth@gmail.com>
2015-01-24 18:16:53 -0500

    Added hideable column functionality

--- rig.js
+++ rig.js
@@ -1,8 +1,17 @@
-var breakpoint = 600,
-	mobilePositionIndex = 0;
+// Breakpoints
+var smallBreakpoint = 600,
+	largeBreakpoint = 1024,
+// initial position on mobile screens, zero-indexed from left to right
+	initMobilePositionIndex = 0;
 
 
+var mobilePositionIndex = initMobilePositionIndex;
+
+// bindings and init setup – runs only once on ready
 function initRig(){
+	$('.rig').addClass('rig-init');
+	setTimeout(function(){$('.rig').removeClass('rig-init')}, 50);
+
 	$('.rig').wrapInner('<div class="rig-wrapper"></div>');
 
 	$('.rig-forward').click(function(){
@@ -18,31 +27,50 @@ function initRig(){
 			updateMobilePosition();
 		};
 	});
+
+	$('.rig-hide-right-panel').click(function(){
+		$('.rig-fixed:not(.rig-collapsed), .rig-flex:not(.rig-collapsed)').eq(-1).addClass('rig-collapsed');
+		rig();
+	});
 };
 
+
+// everything that runs to format the layout – triggered by resize
 function rig(){
 
-	if($(window).width() > breakpoint){
+	if($(window).width() > smallBreakpoint){
 		// desktop/tablet
 
-		$('.rig, .rig-sidebar, .rig-body, .rig-wrapper').removeAttr('style');
+		$('.rig, .rig-fixed, .rig-flex, .rig-wrapper').removeAttr('style');
 
-		var totalSidebarWidth = 0;
+		var visibleFixedWidth = 0,
+			totalFixedWidth = 0;
 
-		$('.rig-sidebar').each(function(index) {
-			totalSidebarWidth += parseInt($(this).width(), 10);
+		$('.rig-fixed:not(.rig-collapsed)').each(function(index) {
+			visibleFixedWidth += parseInt($(this).width(), 10);
 		});
 
-		$('.rig-body').width( ($(window).width() - totalSidebarWidth) / $('.rig-body').length );
+		$('.rig-fixed').each(function(index) {
+			totalFixedWidth += parseInt($(this).width(), 10);
+		});
+
+		var rigFlexWidth = ($(window).width() - visibleFixedWidth) / $('.rig-flex:not(.rig-collapsed)').length;
+
+		$('.rig-wrapper').width(totalFixedWidth + (rigFlexWidth * $('.rig-flex').length) );
+
+		$('.rig-flex:not(.rig-collapsed)').width(rigFlexWidth);
+
+		$('.rig-flex.rig-collapsed').width(($(window).width() - visibleFixedWidth) / $('.rig-flex').length);
+
 
 	}else{
 		// mobile
 
-		$('.rig', '.rig-sidebar, .rig-body, .rig-wrapper').removeAttr('style');
+		$('.rig', '.rig-fixed, .rig-flex, .rig-wrapper').removeAttr('style');
 
-		$('.rig-wrapper').width($('.rig-sidebar, .rig-body').length * $(window).width());
+		$('.rig-wrapper').width($('.rig-fixed, .rig-flex').length * $(window).width());
 
-		$('.rig, .rig-sidebar, .rig-body').width($(window).width());
+		$('.rig, .rig-fixed, .rig-flex').width($(window).width());
 
 		$('.rig-wrapper').css('left', $(window).width() * -1)
 
@@ -51,8 +79,8 @@ function rig(){
 
 
 function isMobile(){
-	console.log($(window).width() < breakpoint);
-	return $(window).width() < breakpoint;
+	console.log($(window).width() < smallBreakpoint);
+	return $(window).width() < smallBreakpoint;
 };
 
 function updateMobilePosition(){
@@ -71,14 +99,15 @@ $(document).ready(function(){
 
 var resizeTimeout;
 $(window).resize(function(){
-	$('.rig').addClass('resizing');
+	$('.rig').addClass('rig-resizing');
 
 	rig();
 	updateMobilePosition();
 
+	// Timeouts used to catch the end of the resize event
 	clearTimeout(resizeTimeout);
 	resizeTimeout = setTimeout(function(){
-		$('.rig').removeClass('resizing');
+		$('.rig').removeClass('rig-resizing');
 		console.log('resize done');
 	}, 50);
 
